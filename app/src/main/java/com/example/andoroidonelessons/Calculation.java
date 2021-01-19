3b2295a6d1bf6ff363db908d7f4e17848527358e
Rassell <miraqq55@gmail.com>
2021-01-19 23:34:13 +0100

    Lesson3

---
+++ app/src/main/java/com/example/andoroidonelessons/Calculation.java
@@ -0,0 +1,192 @@
+package com.example.andoroidonelessons;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class Calculation {
+    private final String number0 = "0";
+    private final String number1 = "1";
+    private final String number2 = "2";
+    private final String number3 = "3";
+    private final String number4 = "4";
+    private final String number5 = "5";
+    private final String number6 = "6";
+    private final String number7 = "7";
+    private final String number8 = "8";
+    private final String number9 = "9";
+    private final String symbolPoi = ".";
+    private final char symbolPlus = '+';
+    private final char symbolMin = '-';
+    private final char symbolMul = '*';
+    private final char symbolDiv = '=';
+    private final String error0 = "ERROR: деление на ноль";
+    private double firstNumber = 0;
+    private double secondNumber = 0;
+    private String number = "";
+    private char usingSymbol;
+    private boolean isSymbol = false;
+    private double resultNumber = 0;
+    private int countPoint = 0;
+
+
+    public int getCountPoint() {
+        return countPoint;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getNumber0() {
+        return number0;
+    }
+
+    public String getNumber1() {
+        return number1;
+    }
+
+    public String getNumber2() {
+        return number2;
+    }
+
+    public String getNumber3() {
+        return number3;
+    }
+
+    public String getNumber4() {
+        return number4;
+    }
+
+    public String getNumber5() {
+        return number5;
+    }
+
+    public String getNumber6() {
+        return number6;
+    }
+
+    public String getNumber7() {
+        return number7;
+    }
+
+    public String getNumber8() {
+        return number8;
+    }
+
+    public String getNumber9() {
+        return number9;
+    }
+
+    public String getSymbolPoi() {
+        return symbolPoi;
+    }
+
+    public char getSymbolPlus() {
+        return symbolPlus;
+    }
+
+    public char getSymbolMin() {
+        return symbolMin;
+    }
+
+    public char getSymbolMul() {
+        return symbolMul;
+    }
+
+    public char getSymbolDiv() {
+        return symbolDiv;
+    }
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+
+    public char getUsingSymbol() {
+        return usingSymbol;
+    }
+
+    public boolean isSymbol() {
+        return isSymbol;
+    }
+
+    public double getResultNumber() {
+        return resultNumber;
+    }
+
+    public String getError0() {
+        return error0;
+    }
+
+    public void addNumber(String useNumber) {
+        number = number + useNumber;
+    }
+
+    public void createFirstAndSecondNumber() {
+        if (number.equals("")) {
+            number = "0";
+        }
+        if (isSymbol) {
+            secondNumber = Double.parseDouble(number);
+        } else {
+            firstNumber = Double.parseDouble(number);
+        }
+
+    }
+
+    public void chooseSymbol(char symbol) {
+        usingSymbol = symbol;
+        isSymbol = true;
+        number = "";
+    }
+
+    public void result() {
+        switch (usingSymbol) {
+            case symbolPlus:
+                resultNumber = firstNumber + secondNumber;
+                break;
+            case symbolMin:
+                resultNumber = firstNumber - secondNumber;
+                break;
+            case symbolMul:
+                resultNumber = firstNumber * secondNumber;
+                break;
+            case symbolDiv:
+                if (secondNumber == 0) {
+                    number = error0;
+                } else {
+                    resultNumber = firstNumber / secondNumber;
+                }
+                break;
+        }
+    }
+
+    public void showResult() {
+        NumberFormat nf = new DecimalFormat("#.##########");
+        if (number.equals(error0)) {
+            isSymbol = false;
+
+        } else {
+            number = String.format(Locale.getDefault(), "%s", (nf.format(resultNumber)));
+            isSymbol = false;
+            createFirstAndSecondNumber();
+        }
+    }
+
+    public void clearCalculator() {
+        number = "";
+        isSymbol = false;
+        firstNumber = 0;
+        secondNumber = 0;
+        countPoint = 0;
+        resultNumber = 0;
+    }
+
+    public boolean counterPoints() {
+        return number.indexOf('.') > -1;
+    }
+}
--- app/src/main/java/com/example/andoroidonelessons/MainActivity.java
+++ app/src/main/java/com/example/andoroidonelessons/MainActivity.java
@@ -1,14 +1,267 @@
 package com.example.andoroidonelessons;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private final Calculation calculation = new Calculation();
+    private TextView calculator;
+    private Button button0;
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
+    private Button button5;
+    private Button button6;
+    private Button button7;
+    private Button button8;
+    private Button button9;
+    private Button buttonPlus;
+    private Button buttonMin;
+    private Button buttonMul;
+    private Button buttonDiv;
+    private Button buttonEqu;
+    private Button buttonPoi;
+    private Button buttonPer;
+    private Button buttonClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.number0:
+                calculation.addNumber(calculation.getNumber0());
+                setTextCalculator(calculation.getNumber());
+                break;
+            case R.id.number1:
+                calculation.addNumber(calculation.getNumber1());
+                setTextCalculator(calculation.getNumber());
+                break;
+            case R.id.number2:
+                calculation.addNumber(calculation.getNumber2());
+                setTextCalculator(calculation.getNumber());
+                break;
+            case R.id.number3:
+                calculation.addNumber(calculation.getNumber3());
+                setTextCalculator(calculation.getNumber());
+                break;
+            case R.id.number4:
+                calculation.addNumber(calculation.getNumber4());
+                setTextCalculator(calculation.getNumber());
+                break;
+            case R.id.number5:
+                calculation.addNumber(calculation.getNumber5());
+                setTextCalculator(calculation.getNumber());
+                break;
+            case R.id.number6:
+                calculation.addNumber(calculation.getNumber6());
+                setTextCalculator(calculation.getNumber());
+                break;
+            case R.id.number7:
+                calculation.addNumber(calculation.getNumber7());
+                setTextCalculator(calculation.getNumber());
+                break;
+            case R.id.number8:
+                calculation.addNumber(calculation.getNumber8());
+                setTextCalculator(calculation.getNumber());
+                break;
+            case R.id.number9:
+                calculation.addNumber(calculation.getNumber9());
+                setTextCalculator(calculation.getNumber());
+                break;
+            case R.id.symbol_poi:
+                calculation.addNumber(calculation.getSymbolPoi());
+                setTextCalculator(calculation.getNumber());
+                blockButtonPoint(calculation.counterPoints());
+                break;
+            case R.id.symbol_plus:
+                blockButtonPoint(false);
+                calculation.createFirstAndSecondNumber();
+                calculation.chooseSymbol(calculation.getSymbolPlus());
+                break;
+            case R.id.symbol_min:
+                blockButtonPoint(false);
+                calculation.createFirstAndSecondNumber();
+                calculation.chooseSymbol(calculation.getSymbolMin());
+                break;
+            case R.id.symbol_mul:
+                blockButtonPoint(false);
+                calculation.createFirstAndSecondNumber();
+                calculation.chooseSymbol(calculation.getSymbolMul());
+                break;
+            case R.id.symbol_div:
+                blockButtonPoint(false);
+                calculation.createFirstAndSecondNumber();
+                calculation.chooseSymbol(calculation.getSymbolDiv());
+                break;
+            case R.id.symbol_equ:
+                calculation.createFirstAndSecondNumber();
+                calculation.result();
+                calculation.showResult();
+                setTextCalculator(calculation.getNumber());
+                if (calculation.getNumber().equals(calculation.getError0())) {
+                    blockButtons(false);
+                }
+                blockButtonPoint(calculation.counterPoints());
+                break;
+            case R.id.symbol_clear:
+                blockButtons(true); //разблокировка кнопок, если были какие-то заблокированы
+                calculation.clearCalculator();
+                setTextCalculator(calculation.getNumber());
+                break;
+        }
+    }
+
+    private void blockButtons(boolean block) {
+        button0.setEnabled(block);
+        button1.setEnabled(block);
+        button2.setEnabled(block);
+        button3.setEnabled(block);
+        button4.setEnabled(block);
+        button5.setEnabled(block);
+        button6.setEnabled(block);
+        button7.setEnabled(block);
+        button8.setEnabled(block);
+        button9.setEnabled(block);
+        buttonPoi.setEnabled(block);
+        buttonPer.setEnabled(block);
+        buttonPlus.setEnabled(block);
+        buttonMin.setEnabled(block);
+        buttonMul.setEnabled(block);
+        buttonDiv.setEnabled(block);
+        buttonEqu.setEnabled(block);
+    }
+
+    private void blockButtonPoint(boolean block) {
+        buttonPoi.setEnabled(!block);
+    }
+
+    private void initViews() {
+        calculator = findViewById(R.id.calc);
+        initButton0();
+        initButton1();
+        initButton2();
+        initButton3();
+        initButton4();
+        initButton5();
+        initButton6();
+        initButton7();
+        initButton8();
+        initButton9();
+        initButtonPlus();
+        initButtonMinus();
+        initButtonMultiply();
+        initButtonDivision();
+        initButtonEqually();
+        initButtonPoint();
+        initButtonPercent();
+        initButtonClear();
+    }
+
+    private void setTextCalculator(String number) {
+        calculator.setText(String.format(Locale.getDefault(), "%s", number));
+    }
+
+    private void initButton0() {
+        button0 = findViewById(R.id.number0);
+        button0.setOnClickListener(this);
+    }
+
+    private void initButton1() {
+        button1 = findViewById(R.id.number1);
+        button1.setOnClickListener(this);
+    }
+
+    private void initButton2() {
+        button2 = findViewById(R.id.number2);
+        button2.setOnClickListener(this);
+    }
+
+    private void initButton3() {
+        button3 = findViewById(R.id.number3);
+        button3.setOnClickListener(this);
+    }
+
+    private void initButton4() {
+        button4 = findViewById(R.id.number4);
+        button4.setOnClickListener(this);
+    }
+
+    private void initButton5() {
+        button5 = findViewById(R.id.number5);
+        button5.setOnClickListener(this);
+    }
+
+    private void initButton6() {
+        button6 = findViewById(R.id.number6);
+        button6.setOnClickListener(this);
+    }
+
+    private void initButton7() {
+        button7 = findViewById(R.id.number7);
+        button7.setOnClickListener(this);
+    }
+
+    private void initButton8() {
+        button8 = findViewById(R.id.number8);
+        button8.setOnClickListener(this);
+    }
+
+    private void initButton9() {
+        button9 = findViewById(R.id.number9);
+        button9.setOnClickListener(this);
+    }
+
+    private void initButtonPoint() {
+        buttonPoi = findViewById(R.id.symbol_poi);
+        buttonPoi.setOnClickListener(this);
+    }
+
+    private void initButtonPercent() {
+        buttonPer = findViewById(R.id.symbol_per);
+        buttonPer.setOnClickListener(this);
+    }
+
+    private void initButtonClear() {
+        buttonClear = findViewById(R.id.symbol_clear);
+        buttonClear.setOnClickListener(this);
+    }
+
+    private void initButtonPlus() {
+        buttonPlus = findViewById(R.id.symbol_plus);
+        buttonPlus.setOnClickListener(this);
+    }
+
+    private void initButtonMinus() {
+        buttonMin = findViewById(R.id.symbol_min);
+        buttonMin.setOnClickListener(this);
+    }
+
+    private void initButtonMultiply() {
+        buttonMul = findViewById(R.id.symbol_mul);
+        buttonMul.setOnClickListener(this);
+    }
+
+    private void initButtonDivision() {
+        buttonDiv = findViewById(R.id.symbol_div);
+        buttonDiv.setOnClickListener(this);
+    }
+
+    private void initButtonEqually() {
+        buttonEqu = findViewById(R.id.symbol_equ);
+        buttonEqu.setOnClickListener(this);
+
     }
 }
